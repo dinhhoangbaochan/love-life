7e2831d1b1c1208fad24ee44da652ecbb9f34276
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-12-21 18:50:39 +0700

    add basic info to conf file and set global love module

--- conf.lua
+++ conf.lua
@@ -1,4 +1,7 @@
 function love.conf(t)
+  -- Window Configuration --
+  t.window.title = "Love Life"
+
   t.console = true
   t.version = "11.5"    -- LOVE2D version
 end
--- main.lua
+++ main.lua
@@ -1,10 +1,1 @@
 _G.love = require("love")
-
-function love.load()
-  print("hello")
-end 
-
-
-function love.draw()
-  love.graphics.print("Let's go")
-end
