3b3ec96563224a3a0bc749e1b7cc3d4055d55830
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-12-21 16:42:41 +0700

    init project

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+  t.console = true
+  t.version = "11.5"    -- LOVE2D version
+end
---
+++ main.lua
@@ -0,0 +1,10 @@
+_G.love = require("love")
+
+function love.load()
+  print("hello")
+end 
+
+
+function love.draw()
+  love.graphics.print("Let's go")
+end
